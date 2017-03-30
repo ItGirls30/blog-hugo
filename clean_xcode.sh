02272defe91642219e5cc0c6a1d13d03d3417a11
ItGirls30 <itgirls30@gmail.com>
2017-03-30 23:54:04 +0800

    Update

---
+++ clean_xcode.sh
@@ -0,0 +1,7 @@
+rm -rt ~/Library/Developer/Xcode/iOS DeviceSupport
+rm -rt ~/Library/Application Support/iPhone Simulator
+rm -rt ~/Library/Application Support/iPhone Simulator/6.1/tmp
+rm -rt ~/Library/Application Support/iPhone Simulator/6.1/Applications
+rm -rt ~/Library/Developer/Xcode/Archives
+rm -rt ~/Library/Developer/Xcode/DerivedData
+rm -rt ~/Library/Developer/Shared/Documentation/DocSets
